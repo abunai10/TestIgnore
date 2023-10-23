c02ecb312de64610a25bb4ab68ca81dd67919ef8
abunai10 <abunai10@proton.me>
2023-10-23 13:55:09 -0400

    Add priest

---
+++ Rotations/Priest.lua
@@ -0,0 +1,16 @@
+local Unlocker, Caffeine, Project = ...
+
+-- This is only for retail
+if Caffeine:IsClassic() then return end
+
+local spec_index = GetSpecialization()
+local spec_id = GetSpecializationInfo(spec_index)
+
+-- Initial Spec (No spec)
+if spec_id ~= 1446 then return end
+
+local spells        = Project.Spells
+
+local Module        = Caffeine.Module:New("priest")
+
+Caffeine:Register(Module)
